a17815c019ad7ae0cc65a4a2023283dd1a1319ee
victorpigo <50111538+victorpigo@users.noreply.github.com>
2019-05-02 20:41:37 +0200

    Add files via upload

---
+++ afegeix-nou-usuari-local.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+cert=1
+fals=0
+
+identificador_usuari=`id -u`
+usuari_existent=cert
+
+
+if [ $identificador_usuari -ne 0 ];then
+   echo Has de ser root per afegir un usuari
+   exit 1
+else
+    #Bucle que es repetirà quan l'usuari ja es trobi en el sistema
+    while [ $usuari_existent == "cert" ]
+      do
+
+	echo "Escriu el nom de l'usuari: " $1
+
+
+	echo "Escriu el nom complet de l'usuari: " $2
+
+
+	 
+echo "Inserta la longitud de la contrasenya:"
+read contrasenya
+for p in $(seq 1);
+do
+     openssl rand -base64 48 | cut -c1-$contrasenya
+done
+
+
+
+
+	useradd -c ${1} -m ${1} > /dev/null 2>&1
+
+
+	if [ $? -gt 0 ]; then
+		echo "l'usuari $1 ja existeix"
+	else
+	    usuari_existent=fals
+
+	    echo -e "${contrasenya}\n${contrasenya}" | passwd ${1} > /dev/null 2>&1
+
+
+	    if [ $? -eq 0 ];then
+	    	echo "L'usuari $nom_usuari s'ha creat correctament"
+
+
+	    	passwd -e ${1}
+
+
+	    	echo "Usuari: $1"
+
+	    	echo "Longitud Contrasenya: $contrasenya"
+
+	    	echo "Host: `hostname`"
+	   else
+		echo "Contrasenya no especificada"
+		exit 1
+	  fi
+	fi
+  done
+fi
